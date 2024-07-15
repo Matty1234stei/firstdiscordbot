c9b8b6cc11f3f473fcba82cb9d63971d5988218f
Matthew Steinmetz <mattygsteinmetz@gmail.com>
2024-07-15 11:27:11 -0400

    7/9/24 We moved the code into another file

--- src/extensions/coin_value.py
+++ src/extensions/coin_value.py
@@ -1,3 +1,4 @@
+from discord.ext import tasks
 from discord.ext.commands import Cog, Bot, hybrid_command, Context
 
 from utils.bot import MyBot
@@ -8,6 +9,14 @@ class CoinValueCog(Cog):
     def __init__(self, bot: MyBot):
         self.bot = bot
         self.cg = bot.cg
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print(1)
 
     @hybrid_command()
     async def get_coin_value(self, ctx: Context, coin: str) -> None:
